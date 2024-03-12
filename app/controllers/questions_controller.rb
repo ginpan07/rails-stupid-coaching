b33e0cbb2ce652c43a3b141142bb2ade06b48bb0
ginpan <zgpan7@gmail.com>
2024-03-12 20:47:50 +1100

    Done? Continuing the testing bit

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,19 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @question = params[:question]
+
+    if params[:question] == "I am going to work"
+      @question
+      @answer = "Great!"
+    elsif params[:question].end_with?("?")
+      @question
+      @answer = "Silly question, get dressed and go to work!"
+    else
+      @question
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
