a9b71b0f41ad72f8e6563355133a66e3947d8a95
ginpan <zgpan7@gmail.com>
2024-03-12 22:36:05 +1100

    DOne

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,7 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  # driven_by :headless_chrome # Update this line
+  driver = ENV['DRIVER'] ? ENV['DRIVER'].to_sym : :headless_chrome
+  driven_by :selenium, using: driver, screen_size: [1400, 1400]
 end
